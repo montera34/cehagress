b3821f2683401bd7e4c32745cdaa0d498767bc8c
skotperez <voraginebramante@gmail.com>
2013-01-24 17:00:42 +0100

    Eventos loop in sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,23 @@
 <div id="margen" class="span3">
+<?php // CALENDAR
+$args = array(
+	'post_type' => 'evento',
+);
+$the_query = new WP_Query( $args );
+if ( $the_query->have_posts() ) { ?>
+
 	<h2 class="tit2">Calendario</h2>
 	<ul class="unstyled">
+
+	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
 		<li class="topslim">
-			<h3 class="tit3 fontup">Se abre el plazo de inscripci'on</h3>
-			<p>Desde el 1 de enero de 2013 se podr'an presentar sus propuestas.</p>
+			<h3 class="tit3 fontup"><a href="<?php the_permalink(); ?>" title="Más información sobre el evento" rel="bookmark"><?php the_title(); ?></a></h3>
+			<p><?php the_excerpt_rss(); ?></p>
 		</li>
-		<li class="topslim">
-			<h3 class="tit3 fontup">Se abre el plazo de inscripci'on</h3>
-			<p>Desde el 1 de enero de 2013 se podr'an presentar sus propuestas.</p>
-		</li>
+	<?php endwhile; ?>
 	</ul>
+<?php } else {
+	// no eventos
+} ?>
 </div><!-- #sidebar -->
