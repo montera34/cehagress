d85724e9138863a1db1805b0b1944adb793e936a
skotperez <voraginebramante@gmail.com>
2013-02-04 14:32:33 +0100

    breadcrumbs style changes

--- breadcrumbs.php
+++ breadcrumbs.php
@@ -1,4 +1,4 @@
-<ul class="breadcrumbs unstyled">
+<ul class="breadcrumbs unstyled inline">
 <?php
 if ( is_home() ) {
 // if home
@@ -6,21 +6,21 @@ if ( is_home() ) {
 
 } elseif ( is_page() ) {
 // if page ?>
-	<li><a href="<?php echo $genvars['blogurl']; ?>">Inicio</a> <span class="divider">/</span></li>
+	<li><a href="<?php echo $genvars['blogurl']; ?>">Inicio</a> <span class="divider">&rsaquo;</span></li>
 
 
 <?php // end if page
 } elseif ( is_single() && get_post_type( $post->ID ) == 'evento' ) {
 // if evento post type ?>
-	<li><a href="<?php echo $genvars['blogurl']; ?>">Inicio</a> <span class="divider">/</span></li>
-	<li><a href="<?php echo $genvars['blogurl']; ?>">Calendario</a> <span class="divider">/</span></li>
-	<li><?php the_title(); ?>"</li>
+	<li><a href="<?php echo $genvars['blogurl']; ?>">Inicio</a> <span class="divider">&rsaquo;</span></li>
+	<li><a href="<?php echo $genvars['blogurl']; ?>">Calendario</a> <span class="divider">&rsaquo;</span></li>
+	<li><?php the_title(); ?></li>
 
 <?php // end if evento
 } elseif ( is_single() && get_post_type( $post->ID ) == 'post' ) {
 // if post ?>
-	<li><a href="<?php echo $genvars['blogurl']; ?>">Inicio</a> <span class="divider">/</span></li>
-	<li><a href="<?php echo $genvars['blogurl']; ?>">Noticias</a> <span class="divider">/</span></li>
+	<li><a href="<?php echo $genvars['blogurl']; ?>">Inicio</a> <span class="divider">&rsaquo;</span></li>
+	<li><a href="<?php echo $genvars['blogurl']; ?>">Noticias</a> <span class="divider">&rsaquo;</span></li>
 	<li><?php the_title(); ?>"</li>
 
 <?php // end if post
