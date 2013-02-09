77b687aa497233e72e1e07b64c0ec9e03a68a879
skotperez <voraginebramante@gmail.com>
2013-02-09 17:11:40 +0100

    social sharing stuff in single

--- loop.single.php
+++ loop.single.php
@@ -5,14 +5,17 @@
 		wp_link_pages( array( 'before' => '<section><div class="art-nav">P&aacute;ginas: ', 'after' => '</div></section>' ) ); ?>
 	</div>
 	<div class="art-meta row muted">
-		<div class="span3 art-meta-col unstyled">
+		<div class="span7 art-meta-col unstyled">
 		<ul class="unstyled">
 			<li>Por <strong><?php the_author_posts_link(); ?>, </strong>el <strong><?php the_time('F d, Y') ?></strong></li>
 			<li>Archivado en: <strong><?php the_category(', '); ?></strong>, <?php the_tags('<span class="tags">',', ','</span>'); ?></li>
+			<li><i class="icon-comment"></i> <?php comments_popup_link('Ningún  comentarios', '1 comentarios', '% comentarios'); ?></li>
 		</ul>
 		</div>
-		<div class="span6 art-meta-col">
-			<?php comments_popup_link('0', '1', '%'); ?> <i class="icon-comment"></i>
+		<div class="span2 art-meta-col">
+			<ul class="unstyled" style="margin-top: 3px;">
+				<?php include "social.php"; ?>
+			</ul>
 		</div>
 	</div>
 </div>
---
+++ social.php
@@ -0,0 +1,41 @@
+<?php // twitter ?>
+<li>
+<a href="https://twitter.com/share" class="twitter-share-button" data-lang="fr">Tweeter</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</li>
+
+<?php // facebook ?>
+<li>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-send="true" data-layout="button_count" data-width="144" data-show-faces="false"></div>
+</li>
+
+<?php // g+ ?>
+<!-- Place this tag where you want the +1 button to render. -->
+<li>
+<div class="g-plusone" data-size="medium"></div>
+
+<!-- Place this tag after the last +1 button tag. -->
+<script type="text/javascript">
+  window.___gcfg = {lang: 'fr'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+</li>
+
+<?php // linkedin ?>
+<li>
+<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/Share" data-counter="right"></script>
+</li>
