0538ee389b7cb6ea2945986d6b8501d67add35f0
skotperez <voraginebramante@gmail.com>
2013-10-14 13:12:31 +0200

    colaboradores column in footer and caja rural image file

--- footer.php
+++ footer.php
@@ -12,12 +12,16 @@ require_once( get_stylesheet_directory(). '/general-vars.php' );
 			<p><a href="http://uclm.es" title="Universidad de Castilla- La Mancha"><img src="<?php echo $genvars['blogtheme']; ?>/images/organiza.uclm.png" alt="Universidad de Castilla- La Mancha" /></a></p>
 			<p><a href="http://uclm.es/ceclm" title="Centro de Estudios de Castilla- La Mancha"><img src="<?php echo $genvars['blogtheme']; ?>/images/organiza.ceclm.png" alt="Centro de Estudios de Castilla- La Mancha" /></a></p>
 			</div>
+			<div class="span2">
+			<h3 class="tit3">Colaboradores:</h3>	
+			<p><a href="http://www.cajarural.com" title="Caja Rural"><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.crural.png" alt="Caja Rural" /></a></p>
+			</div>
 			<div class="span3">
 			<h3 class="tit3">Contacto:</h3>
 			<p><i class="icon-envelope icon-white"></i> <a href="/informacion-practica/contacto/"><small>Envíanos un correo</small></a></p>
 			<p><i class="icon-plus-sign icon-white"></i> <a href="https://www.facebook.com/XxCongresoNacionalDeHistoriaDelArte"><small>Facebook</small></a> | <a href="https://twitter.com/xxcongresoceha"><small>Twitter</small></a></p>
 			</div>
-			<div class="span2 offset2">
+			<div class="span2">
 			<h3 class="tit3">Créditos:</h3>
 			<p><small>Diseño: <a href="http://jorgechamorro.es">Jorge Chamorro</a><br />
 			Desarrollo web: <a href="http://montera34.com">montera34</a></small></p>
