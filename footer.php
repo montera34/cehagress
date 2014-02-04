06015c2a220ed60ff9a7571b0172bb7f760cd8cb
skotperez <voraginebramante@gmail.com>
2014-02-04 19:40:03 +0100

    new footer structure and logos

--- footer.php
+++ footer.php
@@ -4,27 +4,57 @@ require_once( get_stylesheet_directory(). '/general-vars.php' );
 ?>
 
 	<div class="row">
-		<div id="epi" class="span9">
+		<div id="epi" class="span9 topfat">
 		<div class="row">
-			<div class="span2">
-			<h3 class="tit3" style="margin-left: 5px;">Organizadores:</h3>	
-			<p><a href="http://www.arteceha.com/CEHA/ceha.html" title="Comité Español de Historia del Arte"><img src="<?php echo $genvars['blogtheme']; ?>/images/organiza.ceha.png" alt="Comité Español de Historia del Arte" /></a></p>
-			<p><a href="http://uclm.es" title="Universidad de Castilla- La Mancha"><img src="<?php echo $genvars['blogtheme']; ?>/images/organiza.uclm.png" alt="Universidad de Castilla- La Mancha" /></a></p>
+			<div class="span4">
+			<h3 class="tit3">Organizadores:</h3>	
+			<ul class="inline">
+				<li><a href="http://www.arteceha.com/CEHA/ceha.html" title="Comité Español de Historia del Arte"><img src="<?php echo $genvars['blogtheme']; ?>/images/organiza.ceha.png" alt="Comité Español de Historia del Arte" /></a></li>
+				<li><a href="http://uclm.es" title="Universidad de Castilla- La Mancha"><img src="<?php echo $genvars['blogtheme']; ?>/images/organiza.uclm.png" alt="Universidad de Castilla- La Mancha" /></a></li>
+			</ul>
 			</div>
-			<div class="span2">
+			<div class="span5">
+			<h3 class="tit3">Patrocinador:</h3>	
+			<ul class="inline">
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/patrocina.greco.2014.png" alt="El Greco 2014" /></li>
+			</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span9">
 			<h3 class="tit3">Colaboradores:</h3>	
-			<p><a href="http://uclm.es/ceclm" title="Centro de Estudios de Castilla- La Mancha"><img src="<?php echo $genvars['blogtheme']; ?>/images/organiza.ceclm.png" alt="Centro de Estudios de Castilla- La Mancha" /></a></p>
-			<p><a href="http://www.uclm.es/organos/vic_empresas/catedras.asp?op=5" title="Cátedras Universidad-Empresa -- Universidad de Castilla- La Mancha"><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.crural.png" alt="Cátedras Universidad-Empresa -- Universidad de Castilla- La Mancha" /></a></p>
+			<ul class="inline">
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.ayuntamiento.toledo.png" alt="Ayuntamiento de Toledo" title="Ayuntamiento de Toledo" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.junta.castilla.la.mancha.png" alt="Junta de Comunidades de Castilla- La Mancha" title="Junta de Comunidades de Castilla- La Mancha" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.ministerio.cultura.png" alt="Ministerio de Cultura y Deporte" title="Ministerio de Cultura y Deporte" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.consorcio.toledo.png" alt="Consorcio de Toledo" title="Consorcio de Toledo" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.arzobispado.toledo.png" alt="Arzobispado de Toledo" title="Arzobispado de Toledo" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.catedral.toledo.png" alt="Excelentísimo Cabildo de la Santa Iglesia Catedral Primada" title="Excelentísimo Cabildo de la Santa Iglesia Catedral Primada" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.museo.ejercito.png" alt="Museo del Ejército" title="Museo del Ejército" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.museo.prado.gif" alt="Museo del Prado" title="Museo del Prado" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.museo.sefardi.png" alt="Museo Sefardí" title="Museo Sefardí" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.museo.greco.png" alt="Museo del Greco" title="Museo del Greco" /></li>
+				<li><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.museo.santa.cruz.png" alt="Museo de Santa Cruz" title="Museo de Santa Cruz" /></li>
+
+				<li><a href="http://uclm.es/ceclm" title="Centro de Estudios de Castilla- La Mancha"><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.ceclm.png" alt="Centro de Estudios de Castilla- La Mancha" /></a></li>
+				<li><a href="http://www.uclm.es/organos/vic_empresas/catedras.asp?op=5" title="Cátedras Universidad-Empresa -- Universidad de Castilla- La Mancha"><img src="<?php echo $genvars['blogtheme']; ?>/images/colabora.crural.png" alt="Cátedras Universidad-Empresa -- Universidad de Castilla- La Mancha" /></a></li>
+			</ul>
 			</div>
-			<div class="span3">
+		</div>
+		<div class="row topslim">
+			<div class="span5">
 			<h3 class="tit3">Contacto:</h3>
-			<p><i class="icon-envelope icon-white"></i> <a href="/informacion-practica/contacto/"><small>Envíanos un correo</small></a></p>
-			<p><i class="icon-plus-sign icon-white"></i> <a href="https://www.facebook.com/XxCongresoNacionalDeHistoriaDelArte"><small>Facebook</small></a> | <a href="https://twitter.com/xxcongresoceha"><small>Twitter</small></a></p>
+			<ul class="inline">
+				<li><i class="icon-envelope"></i> <a href="/informacion-practica/contacto/"><small>Envíanos un correo</small></a></li>
+				<li><i class="icon-plus-sign"></i> <a href="https://www.facebook.com/XxCongresoNacionalDeHistoriaDelArte"><small>Facebook</small></a> | <a href="https://twitter.com/xxcongresoceha"><small>Twitter</small></a></li>
+			</ul>
 			</div>
-			<div class="span2">
+			<div class="span4">
 			<h3 class="tit3">Créditos:</h3>
-			<p><small>Diseño: <a href="http://jorgechamorro.es">Jorge Chamorro</a><br />
-			Desarrollo web: <a href="http://montera34.com">montera34</a></small></p>
+			<ul class="inline">
+				<li><small>Diseño: <a href="http://jorgechamorro.es">Jorge Chamorro</a></li>
+				<li>Desarrollo web: <a href="http://montera34.com">montera34</a></small></li>
+			</ul>
 			</div>
 		</div>
 		</div><!-- #epi -->
