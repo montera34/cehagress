87ed38f0ce70fe02090ffe42427fe840b638a13a
skotperez <voraginebramante@gmail.com>
2014-06-09 19:01:12 +0200

    excerpt remove from sidebar loop

--- loop.sidebar.php
+++ loop.sidebar.php
@@ -1,8 +1,7 @@
 		<li class="topslim">
 			<h3 class="tit3 fontup"><a href="<?php the_permalink(); ?>" title="Más información" rel="bookmark"><?php the_title(); ?></a></h3>
 			<span class="muted">
-				<?php  echo $item_date; ?>
+				<?php echo $item_date; ?>
 			</span>
-			<p><?php the_excerpt_rss(); ?></p>
 		</li>
 
